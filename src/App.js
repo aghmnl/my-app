fd88ff107ef48d7bcbff51fa0c06d70d0313daa1
Agus <aghmnl@gmail.com>
2021-05-22 21:42:44 -0300

    React Bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import agus from "../src/images/Agus.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={agus} alt="Agustín Minetto" />
+        <h1>Agustín Minetto</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
