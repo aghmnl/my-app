832850816dd244c91004c4c07b11c5f7945ed870
Agus <aghmnl@gmail.com>
2021-05-22 23:04:46 -0300

    now is responsive to screen size

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import agus from '../src/images/Agus.png'
 import React from 'react'
-import { Container, Row, Col, Image } from 'react-bootstrap'
+import { Container, Col, Row, Image } from 'react-bootstrap'
 import './App.css'
 
 function App() {
 	return (
-		<Container className="app">
-			<Row>
-				<Col xs={6} md={4}>
-					<Image className="foto" src={agus} />
+		<Container fluid className="principal">
+			<Row className="columnas">
+				<Col sm={5} md={4} lg={3} xl={2}>
+					<Image src={agus} className="foto" />
+				</Col>
+				<Col sm={7} md={8} lg={9} xl={10}>
+					Agustín Minetto
 				</Col>
 			</Row>
 		</Container>
