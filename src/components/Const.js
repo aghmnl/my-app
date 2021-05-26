203aaeabe329d8aaddf1a0a7875148829164af9f
Agus <aghmnl@gmail.com>
2021-05-25 23:09:39 -0300

    Constants in a separate component

---
+++ src/components/Const.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import { SiJavascript, SiTypescript, SiJava, SiReact, SiRedux, SiNodeDotJs, SiAmazonaws, SiPostgresql, SiCss3, SiBootstrap } from 'react-icons/si'
+
+export const lang = {
+	a: (
+		<div>
+			<SiJavascript /> &nbsp; Javascript
+		</div>
+	),
+	b: (
+		<div>
+			<SiTypescript />
+			&nbsp; TypeScript
+		</div>
+	),
+	c: (
+		<div>
+			<SiJava />
+			&nbsp; Java
+		</div>
+	),
+}
+
+export const front = {
+	a: (
+		<div>
+			<SiReact /> &nbsp; React
+		</div>
+	),
+	b: (
+		<div>
+			<SiRedux />
+			&nbsp; Redux
+		</div>
+	),
+}
+
+export const back = {
+	a: (
+		<div>
+			<SiNodeDotJs /> &nbsp; Node.js
+		</div>
+	),
+	b: (
+		<div>
+			<SiAmazonaws />
+			&nbsp; AWS
+		</div>
+	),
+	c: (
+		<div>
+			<SiPostgresql />
+			&nbsp; PostgresSQL
+		</div>
+	),
+}
+
+export const design = {
+	a: (
+		<div>
+			<SiCss3 /> &nbsp; CSS
+		</div>
+	),
+	b: (
+		<div>
+			<SiBootstrap />
+			&nbsp; React-Bootsrap
+		</div>
+	),
+}
+
+export const testing = {
+	a: 'Cypress',
+	b: 'Reflect',
+}
+export const mobile = {
+	a: (
+		<div>
+			<SiReact /> &nbsp; React Native
+		</div>
+	),
+	b: (
+		<div>
+			<SiJava />
+			&nbsp; Java
+		</div>
+	),
+}
--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -1,100 +1,48 @@
 import React from 'react'
 import { Col, ListGroup } from 'react-bootstrap'
-// import { ReactComponent as CypressLogo } from '../images/Cypress.svg'
-import { SiReact, SiJavascript, SiTypescript, SiJava, SiCss3, SiBootstrap, SiRedux, SiNodeDotJs, SiAmazonaws, SiPostgresql } from 'react-icons/si'
+import { lang, front, back, design, testing, mobile } from './Const.js'
+import '../styles/Stack.css'
+
+const row1 = [
+	[lang, 'Languages'],
+	[front, 'FrontEnd'],
+	[back, 'BackEnd'],
+]
+
+const row2 = [
+	[design, 'Design'],
+	[testing, 'Testing'],
+	[mobile, 'Mobile'],
+]
+
+const data = [row1, row2]
 
 function Stack() {
 	return (
 		<div className="section">
 			<div className="sectionTitle">Stack</div>
-			<div className="columns">
-				<Col className="column">
-					<div className="subtitle">Languages</div>
-					<ListGroup variant="flush" className="lista">
-						<ListGroup.Item className="itemLista">
-							<SiJavascript />
-							&nbsp; JavaScript
-						</ListGroup.Item>
-						<ListGroup.Item className="itemLista">
-							<SiTypescript />
-							&nbsp; TypeScript
-						</ListGroup.Item>
-						<ListGroup.Item className="itemLista">
-							<SiJava />
-							&nbsp; Java
-						</ListGroup.Item>
-					</ListGroup>
-					<div className="text"></div>
-				</Col>
-				<Col className="column">
-					<div className="subtitle">FrontEnd</div>
-					<ListGroup variant="flush" className="lista">
-						<ListGroup.Item className="itemLista">
-							<SiReact />
-							&nbsp; React
-						</ListGroup.Item>
-						<ListGroup.Item className="itemLista">
-							<SiRedux />
-							&nbsp; Redux
-						</ListGroup.Item>
-					</ListGroup>
-					<div className="text"></div>
-				</Col>
-				<Col className="column">
-					<div className="subtitle">BackEnd</div>
-					<ListGroup variant="flush" className="lista">
-						<ListGroup.Item className="itemLista">
-							<SiNodeDotJs />
-							&nbsp; Node.js
-						</ListGroup.Item>
-						<ListGroup.Item className="itemLista">
-							<SiAmazonaws /> &nbsp; AWS
-						</ListGroup.Item>
-						<ListGroup.Item className="itemLista">
-							<SiPostgresql /> &nbsp; PostgresSQL
-						</ListGroup.Item>
-					</ListGroup>
-					<div className="text"></div>
-				</Col>
-			</div>
-			<div className="columns">
-				<Col className="column">
-					<div className="subtitle">Design</div>
-					<ListGroup variant="flush" className="lista">
-						<ListGroup.Item className="itemLista">
-							<SiCss3 /> &nbsp; CSS
-						</ListGroup.Item>
-						<ListGroup.Item className="itemLista">
-							<SiBootstrap /> &nbsp; React-Bootsrap
-						</ListGroup.Item>
-					</ListGroup>
-					<div className="text"></div>
-				</Col>
-				<Col className="column">
-					<div className="subtitle">Testing</div>
-					<ListGroup variant="flush" className="lista">
-						<ListGroup.Item className="itemLista">
-							{/* <CypressLogo /> */}
-							Cypress
-						</ListGroup.Item>
-						<ListGroup.Item className="itemLista">Reflect</ListGroup.Item>
-					</ListGroup>
-					<div className="text"></div>
-				</Col>
-				<Col className="column">
-					<div className="subtitle">Mobile</div>
-					<ListGroup variant="flush" className="lista">
-						<ListGroup.Item className="itemLista">
-							<SiReact /> &nbsp; React Native
-						</ListGroup.Item>
-						<ListGroup.Item className="itemLista">
-							<SiJava />
-							&nbsp; Java
-						</ListGroup.Item>
-					</ListGroup>
-					<div className="text"></div>
-				</Col>
-			</div>
+			{data.map((row, id) => {
+				return (
+					<div key={id} className="columns">
+						{row.map((c, i) => {
+							return (
+								<Col key={i} className="column">
+									<div className="subtitle">{c[1]}</div>
+									<ListGroup variant="flush" className="lista">
+										{Object.values(c[0]).map((v, index) => {
+											return (
+												<ListGroup.Item key={index} className="itemLista">
+													{v}
+												</ListGroup.Item>
+											)
+										})}
+									</ListGroup>
+								</Col>
+							)
+						})}
+					</div>
+				)
+			})}
 		</div>
 	)
 }
